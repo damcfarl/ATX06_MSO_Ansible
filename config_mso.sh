937c995c9e55124d89005352d3ce2330de9846c9
David McFarland <damcfarl@cisco.com>
2020-03-31 13:37:26 -0400

    Add files via upload

---
+++ config_mso.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+
+
+/usr/bin/ansible-playbook -i hosts 1-create-tenant.yml
+/usr/bin/ansible-playbook -i hosts 2-create-schema.yml
+/usr/bin/ansible-playbook -i hosts 3-add-sites-schema.yml
+/usr/bin/ansible-playbook -i hosts 4-add-anp-epg-bd-vrf.yml
+/usr/bin/ansible-playbook -i hosts 5-deploy-schema.yml
